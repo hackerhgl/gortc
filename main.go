85ae9bdac5c2f344ee4ca900cb4d44c0d8874ff2
hackerhgl <hackerhgl@gmail.com>
2021-07-18 21:49:52 +0500

    Basic integration

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	app := iris.New()
+	app.Use(iris.Compression)
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.WriteString("OMG VOLLx")
+	})
+
+	app.Listen(":8080")
+	fmt.Println("Server running at port :8080")
+}
