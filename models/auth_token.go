09437f9a00c173069ac3517dfec57b4c0ec91e28
hackerhgl <hackerhgl@gmail.com>
2021-07-25 19:33:59 +0500

    Clean model

--- models/auth_token.go
+++ models/auth_token.go
@@ -1,17 +1,14 @@
 package gortc_models
 
 import (
-	"database/sql"
 	"time"
 )
 
 type AuthToken struct {
-	ID        uint         `gorm:"primarykey" json:"id"`
-	UserID    uint         `json:"userId"`
-	Token     string       `gorm:"not null" json:"token"`
-	IsActive  bool         `gorm:"default:true" json:"isActive"`
-	CreatedAt time.Time    `json:"-"`
-	UpdatedAt time.Time    `json:"-"`
-	DeletedAt sql.NullTime `gorm:"index" json:"-"`
+	ID        uint      `gorm:"primarykey" json:"id"`
+	UserID    uint      `json:"userId"`
+	Token     string    `gorm:"not null" json:"token"`
+	IsActive  bool      `gorm:"default:true" json:"isActive"`
+	CreatedAt time.Time `json:"-"`
 	User      User
 }
